4c4f650e6594638eb17d58107ff910e8cdafa420
justin <justin_yih@hotmail.com>
2020-02-11 12:30:06 -0800

    merged version 1.2

--- game/routes/secure.js
+++ game/routes/secure.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/submit-score', asyncMiddleware(async (req, res, next) => {
   const { email, score } = req.body;
+  console.log(req.body)
   await UserModel.updateOne({ email }, { highScore: score });
   res.status(200).json({ status: 'ok' });
 }));
